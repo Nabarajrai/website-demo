06344e55f3ca5fdeb858b43647cfd95fa8d84b93
nabaraj <nabaraj2055@gmail.com>
2021-04-21 09:26:12 +0545

    Adding bootstrap and reactstrap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Button } from "reactstrap";
 
 const HomePage = () => {
   return (
     <div>
       <h2>Helllow I am Home page !</h2>
+      <Button color="danger">Clike me</Button>
+      <Button color="primary">Clike me</Button>
     </div>
   );
 };
