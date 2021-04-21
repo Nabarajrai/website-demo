35bdf6a9dcaf10e6d1949a33d64fd7f6badccbca
nabaraj <nabaraj2055@gmail.com>
2021-04-21 11:40:46 +0545

    Download Fontawesome

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 const HomePage = () => {
   return (
     <div>
       <h2>Helllow I am Home page !</h2>
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 };
